724e4e708dd33f458d35bd3360761abf6871caaa
Bryce Meyer <bryce@axolotlinteractive.com>
2016-03-10 18:00:01 -0600

    filled out JSONResponseHandler

--- src/main/java/org/anolis/networking/response/JSONResponseHandler.java
+++ src/main/java/org/anolis/networking/response/JSONResponseHandler.java
@@ -7,34 +7,60 @@ import org.json.JSONObject;
 
 /**
  * Created by Quixotical on 10/18/15.
+ *
  */
-public abstract class JSONResponseHandler implements ResponseHandler{
+public abstract class JSONResponseHandler implements ResponseHandler {
+
     /**
      * JSONObject JSONResponse = the converted parseResponse String stored as a JSONObject
      */
-    private JSONObject JSONResponse;
-    private JSONException JSONError;
+    private JSONObject response;
 
-    public abstract void onResponseSuccess(JSONObject response);
+    /**
+     * The error that was generated when we attempted to parse the body
+     */
+    private JSONException error;
 
-    public abstract void onResponseFailure(JSONException responseException);
 
-    @Override
-    public void onConnectionSuccess() {
-        if(JSONError == null) {
-            onResponseSuccess(JSONResponse);
-        }else{
-            onResponseFailure(JSONError);
+    @Override public void onConnectionSuccess() {
+        if (this.error == null) {
+            this.onCallSuccess(this.response);
+        }
+        else {
+            this.onCallFailure(this.error);
         }
     }
 
-    @Override
-    public void parseResponse(String response){
-        try{
-            JSONResponse = new JSONObject(response);
-        }catch(JSONException e){
+    /**
+     *
+     * @param response the raw response received
+     */
+    @Override public final void parseResponse(String response){
+        try {
+            this.response = new JSONObject(response);
+            this.onBackgroundSuccess(this.response);
+        }
+        catch(JSONException e) {
             Log.e("JSONResponseHandler", e.getMessage());
-            JSONError = e;
+            this.error = e;
         }
     }
+
+    /**
+     * Called in the background in order to dispatch the result on the thread that made the call
+     * @param response the parsed response
+     */
+    public abstract void onBackgroundSuccess(JSONObject response);
+
+    /**
+     * Called after the call has returned to the UI Thread
+     * @param response the parsed response
+     */
+    public abstract void onCallSuccess(JSONObject response);
+
+    /**
+     * Called in the background in order to dispatch
+     * @param responseException the error that was received
+     */
+    public abstract void onCallFailure(JSONException responseException);
 }
